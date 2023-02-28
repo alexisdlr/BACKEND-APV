d2a023aaebbff59e9d4cbe1e552c0912d04582ac
Alexis De Leon <alexisdlr3009@hotmail.com>
2023-02-28 09:11:27 -0600

    added headers

--- index.js
+++ index.js
@@ -10,6 +10,14 @@ dotenv.config()
 
 connectDB();
 
+app.use((req, res, next) => {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+	res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+	res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+	next();
+});
+
 app.use(express.json())
 
 app.use('/api/veterinarios', veterinarioRoutes)
